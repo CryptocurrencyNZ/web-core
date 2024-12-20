d2429ada3dcf65ab8497f17e1b7525f4da9ed77b
0xjimmy <me@0xjimmy.xyz>
2024-12-20 14:22:49 +1300

    Pass lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-// Import components
 import HomePage from './components/HomePage'
-
 import './css/style.css'
 
-// Define types for our components if needed
-interface AppProps {
-    // Add any app-level props here
-}
-
-const App: React.FC<AppProps> = () => {
+const App = () => {
     return (
         <Router>
             <div>
