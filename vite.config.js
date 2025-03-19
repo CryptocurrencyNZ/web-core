7837dc13baece86c8b941db5771e7656cb45692b
j7cb1 <jacobb7331@gmail.com>
2025-03-19 19:40:23 +1300

    Fixes to the routing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/web-core/',
+    base: '/',
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx']
     }
-})
+});
